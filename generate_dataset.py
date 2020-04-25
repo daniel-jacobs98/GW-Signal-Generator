dd1dab85ef2d9770d942ae61ae5ff2f91e70bc29
Daniel Jacobs <dj6450@gmail.com>
2020-04-25 23:43:27 +0800

    Fixed signal cutting so merge point is always in the cut signal

--- generate_dataset.py
+++ generate_dataset.py
@@ -26,7 +26,8 @@ import time
 import sys
 import os.path
 from lal import LIGOTimeGPS
-
+from plot_signal import plot_sigs
+import copy
 global signal_len
 signal_len=10000
 
@@ -77,9 +78,15 @@ def get_param_set(sim_params):
 		yield param_set
 
 
+def find_nearest(array,value):
+    idx = np.searchsorted(array, value, side="left")
+    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
+        return idx-1
+    else:
+        return idx
+
 #Generate and return projections of a signal described by param_set onto the Hanford, Livingston, Virgo detectors
 def generate_signal(param_set):
-	print('Masses: ({0}, {1})'.format(param_set['m1'], param_set['m2']))
 	hp, hc = get_td_waveform(approximant='SEOBNRv4',
 							mass1=param_set['m1'],
 							mass2=param_set['m2'],
@@ -90,13 +97,6 @@ def generate_signal(param_set):
 							distance=param_set['dist'],
 							delta_t=1.0/param_set['f'],
 							f_lower=30)
-
-	#Makes the signal start smoothly
-	hp = fade_on(hp)
-	hc = fade_on(hc)
-	hp.resize(signal_len)
-	hc.resize(signal_len)
-
 
 	det_h1 = Detector('H1')
 	det_l1 = Detector('L1')
@@ -110,18 +110,74 @@ def generate_signal(param_set):
 			'L1':sig_l1,
 			'V1':sig_v1}
 
+def cut_sigs(signal_dict):
+	cut_sigs = dict()
+	# rs_sig_dict = copy.deepcopy(signal_dict)
+	# resize_works = True
+	zeroIdxs = {
+		'H1':find_nearest(signal_dict['H1'].sample_times, 0),
+		'L1':find_nearest(signal_dict['L1'].sample_times, 0),
+		'V1':find_nearest(signal_dict['V1'].sample_times, 0)
+	}
+	# for det in ['H1', 'L1', 'V1']:
+	# 	zIdx = zeroIdxs[det]
+	# 	res = rs_sig_dict[det]
+	# 	res.resize(signal_len)
+	# 	if zIdx>res.shape[0]:
+	# 		resize_works=False
+	# 		break
+	# 	else:
+	# 		cut_sigs[det]=res
+
+	# if resize_works:
+	# 	print('Resize')
+	# 	print('H1: {0}\nL1: {1}\nV1: {2}'.format(cut_sigs['H1'].shape[0], cut_sigs['L1'].shape[0], cut_sigs['V1'].shape[0]))
+	# 	return cut_sigs
+
+	print('After res attempt: {0}, {1}, {2}'.format(signal_dict['H1'].shape[0], signal_dict['L1'].shape[0], signal_dict['V1'].shape[0]))
+	for det in ['H1','L1','V1']:
+		zIdx = zeroIdxs[det]
+		sig = signal_dict[det]
+		print(zIdx)
+		startIdx = int(zIdx-(signal_len*0.9))
+		prep_zeros = 0
+		print(startIdx)
+		endIdx = int(zIdx+(signal_len*0.1))
+		ap_zeros = 0
+		print(endIdx)
+		if startIdx<0:
+			prep_zeros = int(startIdx*-1)
+			startIdx = 0
+		if endIdx>sig.shape[0]:
+			ap_zeros = endIdx-sig.shape[0]
+			endIdx = sig.shape[0]-1
+		print('sIdx: {0}, eIdx:  {1}'.format(startIdx, endIdx))
+		print(sig.shape[0])
+		res = sig[startIdx:endIdx]
+		if res.shape[0]!=signal_len:
+			res.prepend_zeros(prep_zeros)
+			res.append_zeros(ap_zeros)
+
+		new_zidx = find_nearest(res.sample_times, 0)
+		print('In' if (new_zidx<endIdx and new_zidx>zIdx) else 'Not in')
+		cut_sigs[det]=res
+	print('Cut')
+	print('H1: {0}\nL1: {1}\nV1: {2}'.format(cut_sigs['H1'].shape[0], cut_sigs['L1'].shape[0], cut_sigs['V1'].shape[0]))
+	return cut_sigs
+
 #Inject a set of signals into Gaussian noise with the given SNR
 def inject_signals_gaussian(signal_dict, inj_snr):
+	resized_sigs = cut_sigs(signal_dict)
 	noise = dict()
 	global sim_params
 	for i, det in enumerate(('H1', 'L1', 'V1')):
 		flow = 30.0
-		delta_f = signal_dict[det].delta_f
+		delta_f = resized_sigs[det].delta_f
 		flen = int(sim_params['sample_freq'] / delta_f) + 1
 		psd = pycbc.psd.aLIGOZeroDetHighPower(flen, delta_f, flow)
-		noise[det] = pycbc.noise.gaussian.noise_from_psd(length=signal_dict[det].sample_times.shape[0],
+		noise[det] = pycbc.noise.gaussian.noise_from_psd(length=resized_sigs[det].sample_times.shape[0],
 														delta_t=1.0/sim_params['sample_freq'], psd=psd)
-		start_time = signal_dict[det].start_time
+		start_time = resized_sigs[det].start_time
 		noise[det]._epoch = LIGOTimeGPS(start_time)
 
 	psds = dict()
@@ -131,12 +187,12 @@ def inject_signals_gaussian(signal_dict, inj_snr):
 	#using dummy strain and psds from the noise, calculate the snr of each signal+noise injection to find the 
 	#network optimal SNR, used for injecting the real signal
 	for det in ('H1', 'L1', 'V1'):
-		delta_f = signal_dict[det].delta_f
-		noise_inj = noise[det].add_into(signal_dict[det])
+		delta_f = resized_sigs[det].delta_f
+		noise_inj = noise[det].add_into(resized_sigs[det])
 		dummy_strain[det] = noise_inj
-		psds[det] = noise_inj.psd(0.5)
+		psds[det] = noise_inj.psd(0.6)
 		psds[det] = pycbc.psd.interpolate(psds[det], delta_f=delta_f)
-		snrs[det] = sigma(htilde=signal_dict[det],
+		snrs[det] = sigma(htilde=resized_sigs[det],
 							psd=psds[det],
 							low_frequency_cutoff=flow)
 	nomf_snr = np.sqrt(snrs['H1']**2 + snrs['L1']**2 + snrs['V1']**2)
@@ -145,7 +201,7 @@ def inject_signals_gaussian(signal_dict, inj_snr):
 
 	#inject signals with the correct scaling factor for the target SNR
 	for det in ('H1', 'L1', 'V1'):
-		noisy_signals[det] = noise[det].add_into(scale_factor*signal_dict[det])
+		noisy_signals[det] = noise[det].add_into(scale_factor*resized_sigs[det])
 	return noisy_signals
 
 if __name__ == '__main__':
@@ -154,8 +210,6 @@ if __name__ == '__main__':
 	parser.add_argument('-cf','--config-file', help='JSON config file')
 	parser.add_argument('-o', '--output', help='Specify output HDF file name')
 	args = vars(parser.parse_args())
-
-	start=time.time()
 
 	#If config file provided, override default ranges with those in the config
 	if args['config_file']!=None:
@@ -172,9 +226,40 @@ if __name__ == '__main__':
 		print('Output file already exists. Please remove this file or use a different file name.')
 		sys.exit(1)
 
+	start=time.time()
+	print('Starting generation of {0} signals...'.format(sim_params['num_signals']))
+
 	sig_list = []
 	param_list = []
 	noisy_sig_list = []
+
+	# test_params = {
+	# 	'm1':38.81,
+	# 	'm2':8.30,
+	# 	'x1':0.36,
+	# 	'x2':-0.81,
+	# 	'snr':18.20,
+	# 	'dist':1839,
+	# 	'ra':2.64,
+	# 	'dec':-0.24,
+	# 	'f':16384,
+	# 	'coa':rand.uniform(sim_params['coa_phase_range'][0], sim_params['coa_phase_range'][1]),
+	# 	'pol':rand.uniform(sim_params['polarisation_range'][0], sim_params['polarisation_range'][1]),
+	# 	'inc':rand.uniform(sim_params['inclination_range'][0], sim_params['inclination_range'][1])
+	# }
+	# sig = generate_signal(test_params)
+	# print(sig)
+	# noisy_sig = inject_signals_gaussian(sig, test_params['snr'])
+	# print(noisy_sig['H1'].shape[0])
+	# print(noisy_sig['L1'].shape[0])	
+	# print(noisy_sig['V1'].shape[0])
+	# fig, axes = plt.subplots(nrows=6)
+	# for i, det in enumerate(['H1', 'L1', 'V1']):
+	# 	axes[i].plot(sig[det].sample_times, sig[det], label=det)
+	# 	axes[i+3].plot(noisy_sig[det].sample_times, noisy_sig[det], label=det)
+	# plt.show()
+	# sys.exit()
+
 	for i in range(0, sim_params['num_signals']):
 		param_list.append(next(get_param_set(sim_params)))
 		sig_list.append(generate_signal(param_list[-1]))
--- plot_signal.py
+++ plot_signal.py
@@ -8,7 +8,6 @@ from matplotlib import pyplot as plt
 import argparse
 import random as rand
 
-#TODO: Fix parameter text positioning
 #TODO: fix figure dimensions
 #TODO: Some waves have merge points not in the signals: When cutting signals shift merger to middle (hanford)
 
@@ -33,27 +32,28 @@ def plot_sigs((h1_strain, h1_time), (l1_strain, l1_time), (v1_strain, v1_time), 
 	plt.gcf().set_size_inches(12, 6, forward=True)
 	return fig
 
-parser = argparse.ArgumentParser(description='Plot a signal given in hdf format')
-parser.add_argument('-i', '--input')
-args = vars(parser.parse_args())
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Plot a signal given in hdf format')
+	parser.add_argument('-i', '--input')
+	args = vars(parser.parse_args())
 
-with h5py.File(args['input'], 'r') as f:
-	print(f.keys())
-	print(f['signals']['Signal parameters'])
-	print(f['signals']['H1 strain'])
-	print(f['signals']['H1 times'])
+	with h5py.File(args['input'], 'r') as f:
+		print(f.keys())
+		print(f['signals']['Signal parameters'])
+		print(f['signals']['H1 strain'])
+		print(f['signals']['H1 times'])
 
 
-	h1_strain = f['signals']['H1 strain']
-	h1_times = f['signals']['H1 times']
-	l1_strain = f['signals']['L1 strain']
-	l1_times = f['signals']['L1 times']
-	v1_strain = f['signals']['V1 strain']
-	v1_times = f['signals']['V1 times']
-	signal_parameters = f['signals']['Signal parameters']
+		h1_strain = f['signals']['H1 strain']
+		h1_times = f['signals']['H1 times']
+		l1_strain = f['signals']['L1 strain']
+		l1_times = f['signals']['L1 times']
+		v1_strain = f['signals']['V1 strain']
+		v1_times = f['signals']['V1 times']
+		signal_parameters = f['signals']['Signal parameters']
 
-	waves_to_plot = rand.sample(range(f['signals']['H1 strain'].shape[0]), k=1)
-	for i in waves_to_plot:
-		fig = plot_sigs((h1_strain[i,:], h1_times[i,:]), (l1_strain[i,:], l1_times[i,:]), (v1_strain[i,:], v1_times[i,:]),
-						signal_parameters[i,:])
-		plt.show()
+		waves_to_plot = rand.sample(range(f['signals']['H1 strain'].shape[0]), k=20)
+		for i in waves_to_plot:
+			fig = plot_sigs((h1_strain[i,:], h1_times[i,:]), (l1_strain[i,:], l1_times[i,:]), (v1_strain[i,:], v1_times[i,:]),
+							signal_parameters[i,:])
+			plt.show()
